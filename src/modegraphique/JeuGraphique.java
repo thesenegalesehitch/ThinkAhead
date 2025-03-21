ed488c680649e789f4691dd7c4371cb5a9241ae3
thesenegalesehitch <aa.ndour5@isepat.edu.sn>
2025-03-21 12:34:31 +0000

    Modification du code pour permettre a la machine de pouvoir jouer

--- src/modegraphique/JeuGraphique.java
+++ src/modegraphique/JeuGraphique.java
@@ -1,10 +1,12 @@
 package modegraphique;
+
 import modeles.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-    public class JeuGraphique extends JFrame {
+
+public class JeuGraphique extends JFrame {
     private Grille grille;
     private JButton[][] bouton;
     private Joueur p1;
@@ -33,50 +35,57 @@ import java.awt.event.ActionListener;
         setVisible(true);
     }
 
-private JPanel grillePanel() {
-    JPanel panel = new JPanel(new GridLayout(bouton.length, bouton.length));
-    for(int i=0;i<bouton.length;i++) {
-        for (int j=0;j<bouton[i].length;j++) {
-            bouton[i][j] = new JButton();
-            bouton[i][j].setText(String.valueOf(grille.getCase(new Position(i, j)).getVal()));
-            bouton[i][j].setEnabled(false); //activer et ou desactiver selon la ligne ou colonne active
-            int finalI=i;
-            int finalJ=j;
-            bouton[i][j].addActionListener(new ActionListener() {
+    private JPanel grillePanel() {
+        JPanel panel = new JPanel(new GridLayout(bouton.length, bouton.length));
+        for(int i=0;i<bouton.length;i++) {
+            for (int j=0;j<bouton[i].length;j++) {
+                bouton[i][j] = new JButton();
+                bouton[i][j].setText(String.valueOf(grille.getCase(new Position(i, j)).getVal()));
+                bouton[i][j].setEnabled(false); //activer et ou desactiver selon la ligne ou colonne active
+                int finalI=i;
+                int finalJ=j;
+                bouton[i][j].addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         jouerCoup(new Position(finalI, finalJ));
                     }
                 });
                 panel.add(bouton[i][j]);
-            }}
+            }
+        }
         activerCaseActive();
-        return panel;}
-private JPanel infoPanel() {
-    JPanel panel = new JPanel();
-    panel.setLayout(new GridLayout(2, 1));
-    JLabel infos = new JLabel("joueur actif: " + joueurActif.getNom());
-    infos.setHorizontalAlignment(SwingConstants.CENTER);
-    panel.add(infos);
-    JLabel scores = new JLabel("scores: " + p1.getNom() + ": " + p1.getScore() + ", " + p2.getNom() + ": " + p2.getScore());
-    scores.setHorizontalAlignment(SwingConstants.CENTER);
-    panel.add(scores);
-    return panel;}
-private void activerCaseActive() {
-    for (int i = 0; i < bouton.length; i++) {
-        for (int j = 0; j < bouton[i].length; j++) {
-            bouton[i][j].setEnabled(false);
-            if (ligneActif && i == posActif && !grille.getCase(new Position(i, j)).caseSelectionnee()) {
-                bouton[i][j].setEnabled(true);
-            } else if (!ligneActif && j == posActif && !grille.getCase(new Position(i, j)).caseSelectionnee()) {
-                bouton[i][j].setEnabled(true);
-                }}
-    } 
-}
-private void jouerCoup(Position position){
-    Case caseJouee = grille.getCase(position);
-    caseJouee.selectionner();
-    joueurActif.ajoutScore(caseJouee.getVal());
+        return panel;
+    }
+
+    private JPanel infoPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 1));
+        JLabel infos = new JLabel("joueur actif: " + joueurActif.getNom());
+        infos.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(infos);
+        JLabel scores = new JLabel("scores: " + p1.getNom() + ": " + p1.getScore() + ", " + p2.getNom() + ": " + p2.getScore());
+        scores.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(scores);
+        return panel;
+    }
+
+    private void activerCaseActive() {
+        for (int i = 0; i < bouton.length; i++) {
+            for (int j = 0; j < bouton[i].length; j++) {
+                bouton[i][j].setEnabled(false);
+                if (ligneActif && i == posActif && !grille.getCase(new Position(i, j)).caseSelectionnee()) {
+                    bouton[i][j].setEnabled(true);
+                } else if (!ligneActif && j == posActif && !grille.getCase(new Position(i, j)).caseSelectionnee()) {
+                    bouton[i][j].setEnabled(true);
+                }
+            } 
+        } 
+    }
+
+    private void jouerCoup(Position position){
+        Case caseJouee = grille.getCase(position);
+        caseJouee.selectionner();
+        joueurActif.ajoutScore(caseJouee.getVal());
         bouton[position.getLigne()][position.getColonne()].setText("A");
         bouton[position.getLigne()][position.getColonne()].setEnabled(false);
         //met a jour l'index actif et soit la ligne soit la colonne
@@ -87,24 +96,43 @@ private void jouerCoup(Position position){
         activerCaseActive();
         //verifie si le jeu est termine
         finPartie();
+
+        // Si c'est le tour de l'ordinateur, faire jouer la machine
+        if (joueurActif instanceof JoueurOrdinateur) {
+            // Utiliser un Timer pour faire une petite pause avant que l'ordinateur joue
+            Timer timer = new Timer(1000, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Position positionMachine = p2.jouer(grille, posActif);
+                    if (positionMachine != null) {
+                        jouerCoup(positionMachine);
+                    }
+                }
+            });
+            timer.setRepeats(false);
+            timer.start(); // Lancer le Timer pour que la machine joue après un délai
+        }
     }
-private void finPartie(){
-    boolean aucunCoupPosible = true;
-    for (int i=0;i<bouton.length;i++) {
-        for (int j=0;j<bouton[i].length;j++) {
-            if (bouton[i][j].isEnabled()) {
-                aucunCoupPosible = false;
-                break;}
-    }
-}
-    if (aucunCoupPosible){
-        JOptionPane.showMessageDialog(this, "fin de la partie \n" +
-                p1.getNom() + ": " + p1.getScore() + " pts\n" +
-                p2.getNom() + ": " + p2.getScore() + " pts");
+
+    private void finPartie(){
+        boolean aucunCoupPosible = true;
+        for (int i=0;i<bouton.length;i++) {
+            for (int j=0;j<bouton[i].length;j++) {
+                if (bouton[i][j].isEnabled()) {
+                    aucunCoupPosible = false;
+                    break;
+                }
+            }
+        }
+        if (aucunCoupPosible){
+            JOptionPane.showMessageDialog(this, "fin de la partie \n" +
+                    p1.getNom() + ": " + p1.getScore() + " pts\n" +
+                    p2.getNom() + ": " + p2.getScore() + " pts");
             System.exit(0);
         }
-}
-public static void main(String[] args){
-     new JeuGraphique(4); //ici on cree une grille 4ligne et 4colonnes
+    }
+
+    public static void main(String[] args){
+        new JeuGraphique(4); //ici on cree une grille 4ligne et 4colonnes
     }
 }
